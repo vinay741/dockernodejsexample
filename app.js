5f736827077754c1600d0b2b51565bab83277a8a
vinay741 <vinaypollakayala@gmail.com>
2024-11-05 14:22:52 +0530

    Both dockerfile and nodejs file

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res) => {
+ res.send("Docker Nodejs Example");
+});
+app.listen(3111,() => {
+ console.log("Server is running on port 3111")
+});
